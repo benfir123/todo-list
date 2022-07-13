b653174afbf1e0250c705d66e53ec40e7fcee4b2
Ben Chanapai <ben_chanapai04@hotmail.com>
2022-07-13 14:04:04 +0700

    Make responsive dropdown menu

--- src/initialLoad.js
+++ src/initialLoad.js
@@ -1,13 +1,18 @@
-import { popUpNewProject, popUpNewToDo } from "./render"
-import { getStorage } from "./handleToDo"
+import { popUpNewProject, popUpNewToDo } from "./render";
+import { getStorage } from "./handleToDo";
 
 export const initialLoad = () => {
+  getStorage();
 
-    getStorage();
-
-    const addToDoButton = document.getElementById('add-to-do')
-    addToDoButton.addEventListener('click', popUpNewToDo)
-    const addProjectButton = document.querySelector('#add-project-btn')
-    addProjectButton.addEventListener('click', popUpNewProject)
-    
-}
+  const addToDoButton = document.getElementById("add-to-do");
+  addToDoButton.addEventListener("click", popUpNewToDo);
+  const addProjectButton = document.querySelector("#add-project-btn");
+  addProjectButton.addEventListener("click", popUpNewProject);
+  const mobileNavButton = document.querySelector(".button-open-nav");
+  const nav = document.querySelector("nav");
+  if (mobileNavButton) {
+    mobileNavButton.addEventListener("click", () => {
+      nav.classList.toggle("height");
+    });
+  }
+};
